4c75c3379cbe93df3c5f3812c05a04de07ccf73e
Akshay Bagai <Akshay.Bagai@ey.com>
2020-06-20 11:19:48 -0400

    initial commit

---
+++ src/main/java/com/test/vaadin/MainView.java
@@ -0,0 +1,46 @@
+package com.test.vaadin;
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.PWA;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.awt.*;
+
+/**
+ * A sample Vaadin view class.
+ * <p>
+ * To implement a Vaadin view just extend any Vaadin component and
+ * use @Route annotation to announce it in a URL as a Spring managed
+ * bean.
+ * Use the @PWA annotation make the application installable on phones,
+ * tablets and some desktop browsers.
+ * <p>
+ * A new instance of this class is created for every new user and every
+ * browser tab/window.
+ */
+@Route
+public class MainView extends VerticalLayout {
+
+    @Autowired
+    private GreetService greetService;
+
+   public MainView() {
+        Button button = new Button("Hello", Notification.show("Hey there!"));
+        add(button);
+   }
+
+
+
+
+
+}
