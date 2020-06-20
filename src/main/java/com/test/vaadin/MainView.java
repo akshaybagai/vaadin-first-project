bf25c89efc293dad540336287cc24a87990283cd
Akshay Bagai <Akshay.Bagai@ey.com>
2020-06-20 13:46:05 -0400

    added a custom plugin

--- src/main/java/com/test/vaadin/MainView.java
+++ src/main/java/com/test/vaadin/MainView.java
@@ -1,20 +1,10 @@
 package com.test.vaadin;
 
-import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.Key;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
-import com.vaadin.flow.component.checkbox.Checkbox;
-import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.test.vaadin.component.PaperComponent;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.server.PWA;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import java.awt.*;
 
 /**
  * A sample Vaadin view class.
@@ -31,12 +21,13 @@ import java.awt.*;
 @Route
 public class MainView extends VerticalLayout {
 
-    @Autowired
-    private GreetService greetService;
-
    public MainView() {
-        Button button = new Button("Hello", Notification.show("Hey there!"));
-        add(button);
+
+       TextArea textArea = new TextArea("Input text here... ");
+       textArea.setMinLength(500);
+
+       add(textArea);
+       add(new PaperComponent());
    }
 
 
---
+++ src/main/java/com/test/vaadin/component/PaperComponent.java
@@ -0,0 +1,10 @@
+package com.test.vaadin.component;
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.JsModule;
+
+@Tag("paper-toggle-button")
+@JsModule("@polymer/paper-toggle-button")
+public class PaperComponent extends Component {
+}
